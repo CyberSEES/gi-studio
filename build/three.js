29807dba81d979441ee8e3657c5ba0e1e25ad2b6
Vance Miller <vbmiller@cs.unc.edu>
2014-05-14 14:19:54 -0400

    Merging master and gh-pages

--- build/three.js
+++ build/three.js
@@ -25577,12 +25577,12 @@ THREE.WebGLRenderer = function ( parameters ) {
 	};
 
 	this.setDepthTest = function ( depthTest ) {
-		if ( _oldDepthTest !== depthTest ) {
 
+		if ( _oldDepthTest !== depthTest ) {
 
 			if ( depthTest ) {
 
-				_gl.disable( _gl.DEPTH_TEST );
+				_gl.enable( _gl.DEPTH_TEST );
 
 			} else {
 
@@ -25649,12 +25649,6 @@ THREE.WebGLRenderer = function ( parameters ) {
 	};
 
 	this.setBlending = function ( blending, blendEquation, blendSrc, blendDst ) {
-
-		// _gl.enable( _gl.BLEND );
-		// _gl.blendEquation( _gl.FUNC_ADD );
-		// _gl.blendFunc( _gl.SRC_ALPHA, _gl.ONE );
-
-		// return;
 
 		if ( blending !== _oldBlending ) {
 
@@ -26871,7 +26865,7 @@ THREE.WebGLRenderer = function ( parameters ) {
 		_gl.clearDepth( 1 );
 		_gl.clearStencil( 0 );
 
-		_gl.disable( _gl.DEPTH_TEST );
+		_gl.enable( _gl.DEPTH_TEST );
 		_gl.depthFunc( _gl.LEQUAL );
 
 		_gl.frontFace( _gl.CCW );
