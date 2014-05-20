7b369cd421369313df32dcabb87f36550a44bb96
Vance Miller <vbmiller@cs.unc.edu>
2014-05-20 08:53:03 -0400

    shrank location input again

--- editor/js/Sidebar.Location.js
+++ editor/js/Sidebar.Location.js
@@ -8,7 +8,7 @@ Sidebar.Location = function ( editor ) {
 
 	var container = new UI.Panel();
 	var locationInputRow = new UI.Panel();
-	var locationInput = new UI.Input().setWidth( '180px' ).setColor( '#444' ).setFontSize( '12px' );
+	var locationInput = new UI.Input().setWidth( '150px' ).setColor( '#444' ).setFontSize( '12px' );
 	var autocomplete = new google.maps.places.Autocomplete( locationInput.dom );
 
     var veginfoPanel = new UI.Panel();
